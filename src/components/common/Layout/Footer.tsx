dbfbcc4ca3d856c8ef7ca5ff38a611e9d57d352e
Ataur Rahman <76764955+ataur39n-sharif@users.noreply.github.com>
2025-06-25 12:37:20 +0600

    Update Footer.tsx

--- src/components/common/Layout/Footer.tsx
+++ src/components/common/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <footer className=' text-light_color text-center bg-secondary_color border-t-[8px] border-primary_color py-[40px]'>
-            <p>&copy; 2024 TREELYT. All Rights Reserved.</p>
+            <p>&copy; 2025 TREELYT. All Rights Reserved.</p>
         </footer>
     );
 };
